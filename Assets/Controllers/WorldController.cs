a9f17bed95352ab885551a8dffc494379175d606
dennym <denny@dennymueller.de>
2018-02-05 17:35:07 +0100

    initital commit

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using System.Reflection;
+// using System;
+
+public class WorldController : MonoBehaviour {
+
+  public GameObject grassPrefab;
+  public GameObject waterPrefab;
+  public GameObject concretePrefab;
+
+  World world;
+
+  void Start () {
+    // Create green world with Grass tiles
+    world = new World();
+
+    for (int x = 0; x < world.Width; x++) {
+      for (int y = 0; y < world.Height; y++) {
+        Tile tile_data = world.GetTileAt(x, y);
+        Vector3 pos = new Vector3(tile_data.X, 0, tile_data.Y);
+
+        GameObject tile_go = Instantiate(grassPrefab, pos, Quaternion.identity);
+        tile_go.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        tile_go.name = "Tile_" + x + "_" + y;
+        tile_go.transform.SetParent(this.transform, true);
+
+        tile_data.RegisterTileTypeChangedCallback( (tile) => { OnTileTypeChange(tile, tile_go); } );
+      }
+    }
+
+    world.RandomizeTiles();
+	}
+
+  void Update () {
+	}
+
+  void OnTileTypeChange(Tile tile_data, GameObject tile_go) {
+    Mesh initialMesh;
+    Mesh swapMesh;
+    Material initialMaterial;
+    Material swapMaterial;
+    GameObject theTarget;
+
+    theTarget = tile_go;
+    initialMesh = tile_go.GetComponent<MeshFilter>().sharedMesh;
+    initialMaterial = tile_go.GetComponent<Renderer>().sharedMaterial;
+
+    if (tile_data.Type == Tile.TileType.Grass) {
+      swapMesh = grassPrefab.GetComponent<MeshFilter>().sharedMesh;
+      swapMaterial = grassPrefab.GetComponent<Renderer>().sharedMaterial;
+
+      theTarget.GetComponent<MeshFilter>().mesh = swapMesh;
+      theTarget.GetComponent<Renderer>().material = swapMaterial;
+    } else if (tile_data.Type == Tile.TileType.Concrete) {
+      swapMesh = concretePrefab.GetComponent<MeshFilter>().sharedMesh;
+      swapMaterial = concretePrefab.GetComponent<Renderer>().sharedMaterial;
+
+      theTarget.GetComponent<MeshFilter>().mesh = swapMesh;
+      theTarget.GetComponent<Renderer>().material = swapMaterial;
+    } else {
+      Debug.LogError("OnTileTypeChange - Unrecognized tile type");
+    }
+  }
+}
---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Tile  {
+  public enum TileType { Grass, Concrete, Water };
+
+  TileType type = TileType.Grass;
+
+  Action<Tile> cbtileTypeChanged;
+
+  public TileType Type {
+    get { return type; }
+    set {
+      TileType oldType = type;
+      type = value;
+      if (cbtileTypeChanged != null && oldType != type) {
+        cbtileTypeChanged(this);
+      }
+    }
+  }
+
+  LooseObject looseObject;
+  InstalledObject installedObject;
+
+  World world;
+  int x;
+  public int X {
+    get { return x; }
+  }
+  int y;
+  public int Y {
+    get { return y; }
+  }
+
+  public Tile( World world, int x, int y) {
+    this.world = world;
+    this.x = x;
+    this.y = y;
+  }
+
+  public void RegisterTileTypeChangedCallback(Action<Tile> callback) {
+    cbtileTypeChanged += callback;
+  }
+
+  public void UnregisterTileTypeChangedCallback(Action<Tile> callback) {
+    cbtileTypeChanged -= callback;
+  }
+
+  // public Material[] Materials;
+
+	// void Start () {
+	// 	// For more sppecific vertices saving approach
+ //    // https://answers.unity.com/questions/139808/creating-a-plane-mesh-directly-from-code.html
+
+ //    GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+ //    plane.transform.position = new Vector3(1f, 20f, 3f);
+ //    plane.GetComponent<MeshRenderer>().material = Materials[0];
+	// }
+
+}
