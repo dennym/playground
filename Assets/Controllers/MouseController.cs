9e01410ebee315c76b523a3d3422f319dd83f392
dennym <denny@dennymueller.de>
2018-02-05 22:04:50 +0100

    add simple camera handling

---
+++ Assets/Controllers/MouseController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+  // TODO: Borderscrolling https://www.youtube.com/watch?v=4HEJGGN2BHk
+
+  public GameObject circleCursor;
+
+  public float turnSpeed = 4.0f;
+  public float panSpeed = 4.0f;
+  public float zoomSpeed = 4.0f;
+
+  private Vector3 mouseOrigin;
+  private bool isPanning;
+  private bool isRotating;
+  private bool isZooming;
+
+  void Update () {
+
+
+    if (Input.GetMouseButtonDown(0)) {
+      mouseOrigin = Input.mousePosition;
+      isRotating = true;
+    }
+
+    if (Input.GetMouseButtonDown(1)) {
+      mouseOrigin = Input.mousePosition;
+      isPanning = true;
+    }
+
+    if (Input.GetMouseButtonDown(2)) {
+      mouseOrigin = Input.mousePosition;
+      isZooming = true;
+    }
+
+    if (!Input.GetMouseButton(0)) isRotating=false;
+    if (!Input.GetMouseButton(1)) isPanning=false;
+    if (!Input.GetMouseButton(2)) isZooming=false;
+
+    if (isRotating) {
+      Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+      Camera.main.transform.RotateAround(Camera.main.transform.position, Camera.main.transform.right, -pos.y * turnSpeed);
+      Camera.main.transform.RotateAround(Camera.main.transform.position, Vector3.up, pos.x * turnSpeed);
+
+    }
+
+    if (isPanning) {
+      Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+      Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
+      Camera.main.transform.Translate(move, Space.Self);
+    }
+
+    if (isZooming) {
+      Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+      Vector3 move = pos.y * zoomSpeed * Camera.main.transform.forward;
+      Camera.main.transform.Translate(move, Space.World);
+    }
+  }
+}
